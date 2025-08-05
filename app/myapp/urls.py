e8b66d8701395d02a5ae5f872e007899ecb04566
matsumoto <tetsuki0721@gmail.com>
2025-08-05 23:33:31 +0900

    requirements.txtにformtools追記 urls.pyにpath追記 views.pyにクラス名追記

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path
-from .views import ChannelListView
+from .views import *
+#ChannelListView
 from . import views
 from myapp.views import health_check
 
 
 urlpatterns = [
-    path('', ChannelListView.as_view(), name='channel_list'), 
-    path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
-    path('health/', health_check, name='health_check')
+    # path('', ChannelListView.as_view(), name='channel_list'), 
+    # path('signup/', views.signup_view, name='signup'),
+    # path('login/', views.login_view, name='login'),
+    # path('health/', health_check, name='health_check')
+    path('signup/',UserSignup.as_view(template_name='signup.html'),name='signup'),
+    path('login/',Login.as_view(template_name='login.html'),name='login'),
+    path('logout/',Logout.as_view(template_name='settings.html'),name='logout'),
+    path('select/role/',RoleSelect.as_view(template_name=''),name='select_role'),
+    path('user/home/',UserHome.as_view(template_name='home.html'),name='user_home'),
+    path('user/setup/skill/',ProfileCreate.as_view(template_name='skill_registration.html'),name='user_setup_skill'),
+    path('user/create/skill',SkillCreate.as_view(template_name='未定'),name='user_create_skill'),
+    path('user/setup/profile/',ProfileCreate.as_view(template_name='profile_create.html'),name='user_setup_profile'),
+    path('user/update/profile/',ProfileUpdate.as_view(template_name='profile_edit.html'),name='user_update_profile'),
+    path('search/',SearchUsers.as_view(template_name='search.html'),name='search'),
+    path('user/detail',UserDitail.as_view(template_name='未定'),name='user_detail'), # HTMLファイル名の確認
+    path('request/send',SendRequest.as_view(),name='request_send'),
+    path('request/list/',RequestList.as_view(template_name='handshake.html'),name='request_list'),
+    path('request/approval/',RequestApproval.as_view(template_name=''),name='request_approval'),
+    path('matching/list/',MatchingList.as_view(template_name='handshake.html'),name='matching_list'),
+    path('contact/user/',Contact.as_view(template_name='未定'),name='contact_user'),
+    path('reviewt/user/',Review.as_view(template_name='未定'),name='reviewt_user'),
 ]
--- app/myapp/views.py
+++ app/myapp/views.py
@@ -1,10 +1,12 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView,CreateView,DeleteView,UpdateView,TemplateView,View
+from django.contrib.auth.views import LoginView,LogoutView
+from formtools.wizard.views import SessionWizardView
 from django.shortcuts import redirect, render
 from .models import Channel
 from .forms import ChannelForm
 from django.http import HttpResponse
 
-
+"""
 # ヘルスチェック
 def health_check(request):
     return HttpResponse("OK", status=200)
@@ -36,4 +38,295 @@ class ChannelListView(ListView):
         if form.is_valid():
             form.save()
         return redirect('channel_list')
+"""
+# サインアップ
+class UserSignup(CreateView):
+    pass
+
+
+
+
+
+
+
+
+
+
+# ログイン
+class Login(LoginView):
+    pass  
+
+
+
+
+
+
+
+
+
+
+
+# ログアウト
+class Logout(LogoutView):
+    pass  
+
+
+
+
+
+
+
+
+
+# 学ぶ・教える選択
+class RoleSelect(TemplateView):
+    def get():
+          
+        pass
+    
+    def post():
+        pass  
+
+
+
+
+
+
+
+
+
+
+# ホーム画面
+class UserHome(TemplateView):
+    def get():
+          
+        pass
+    
+    def post():
+        pass  
+
+
+
+
+
+
+
+
+
+# 初回スキル登録（学ぶ・教える共通）
+class ProfileCreate():
+    def get():
+          
+        pass
+    
+    def post():
+        pass  
+
+
+
+
+
+
+
+
+
+
+# スキル作成
+class SkillCreate():
+    def get():
+          
+        pass
+    
+    def post():
+        pass  
+
+
+
+
+
+
+
+
+
+
+# プロフィール作成(学ぶ・教える共通)
+class ProfileCreate():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# プロフィール編集
+class ProfileUpdate():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# 先生・生徒検索(学ぶ・教える共通)
+class SearchUsers():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# ユーザー詳細画面(学ぶ・教える共通)
+class UserDitail():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# リクエスト送信(学ぶ・教える共通)
+class SendRequest():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# リクエスト一覧(学ぶ・教える共通)
+class RequestList():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# 承認・削除送信(学ぶ・教える共通)
+class RequestApproval():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# マッチング成立一覧(学ぶ・教える共通)
+class MatchingList():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+# コンタクト
+class Contact():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+# 評価
+class Review():
+    def get():
+          
+        pass
+    
+    def post():
+        pass
+
+
+
+
+
+
+
+
+
 
