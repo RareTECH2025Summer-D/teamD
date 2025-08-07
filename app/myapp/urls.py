363421e3a01af9d2341b71e0d683494ef16217d8
mana44RT <monte510gotoh@gmail.com>
2025-08-07 18:41:58 +0900

    [add]スキル登録画面・スキル作成画面・プロフィール作成画面の追加

--- app/myapp/urls.py
+++ app/myapp/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', ChannelListView.as_view(), name='channel_list'), 
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('user/setup/skill', views.skill_setup_view, name='setup_skill'),
+    path('user/create/skill', views.skill_create_view, name='create_skill'),
+    path('user/setup/profile', views.profile_create_view, name='setup_profile'),
     path('health/', health_check, name='health_check')
 ]
